2d1faf190a41731c07a3c534f70f9aa734d1ffd4
ushabindhu <55163082+ushabindhu@users.noreply.github.com>
2019-09-11 08:47:21 +0100

    Updated by usha

--- Selenium/src/abstract1/Abstract1.java
+++ Selenium/src/abstract1/Abstract1.java
@@ -5,8 +5,9 @@ public class Abstract1 extends Subject{
 	void add() {
 		
 		System.out.println("This method is from subject class for abstract1");
+		
 	}
-	
+	// This is Updated by usha 
 	void delete() {
 		
 		System.out.println("This method is from subject class for abstract1");
